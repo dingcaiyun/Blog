75b909a16ac65315e41c49e4efb655ebd6a87af5
dingcaiyun <dingcy@szprism.com>
2018-05-21 09:12:43 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title','Blog') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-   @yield('content')
-</body>
+    @include('layouts._header')
+
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
+  </body>
 </html>
